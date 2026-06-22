0e0fe6c872af3fe7d7517b00c9aa3a48bff0f1fc
ayspin <aylaspinetto@riccarton.school.nz>
2026-06-22 13:12:57 +1200

    Updated view names, merged lifespan_below6 with lifespan_not_endemic (now lifespan_below9), started on UI + parameter queries.

---
+++ moths_database_ui.py
@@ -0,0 +1,58 @@
+
+# Import the libraries to connect to the database and present the information in tables
+import sqlite3
+from tabulate import tabulate
+
+# This is the filename of the database to be used
+DB_NAME = 'moths_database.db'
+
+# This is the SQL to connect to all the tables in the database - only needed if you are using a parameter query (Excellence)
+TABLES = (" moths "
+           "LEFT JOIN origin ON moths.origin_id = origin.origin_id "
+            "LEFT JOIN endemic ON moths.endemic_id = endemic.endemic_id "
+            "LEFT JOIN lifespan ON moths.lifespan_id ")
+
+
+
+
+def print_query(view_name:str):
+    ''' Prints the specified view from the database in a table '''
+    # Set up the connection to the database
+    db = sqlite3.connect(DB_NAME)
+    cursor = db.cursor()
+    # Get the results from the view
+    sql = "SELECT * FROM '" + view_name + "'"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # Get the field names to use as headings
+    field_names = "SELECT name from pragma_table_info('" + view_name + "') AS tblInfo"
+    cursor.execute(field_names)
+    headings = list(sum(cursor.fetchall(),()))
+    # Print the results in a table with the headings
+    print(tabulate(results,headings))
+    db.close()
+
+def print_parameter_query(fields:str, where:str, parameter):
+    """ Prints the results for a parameter query in tabular form. 
+        Only required for Excellence """
+    db = sqlite3.connect(DB_NAME)
+    cursor = db.cursor()
+    sql = ("SELECT " + fields + " FROM " + TABLES + " WHERE " + where)
+    cursor.execute(sql,(parameter,))
+    results = cursor.fetchall()
+    print(tabulate(results,fields.split(",")))
+    db.close()  
+
+make = input('Which make cars do you want to see: ')
+print_parameter_query("model, top_speed", "make = ? ORDER BY top_speed DESC", make)
+
+
+# A IN COMMON NAME
+# LIFESPAN BELOW 9
+# LIFESPAN TOP 5
+# max_wing_offset_and_limit5
+# north_america_lifespan7
+# not_europe_has_e
+# wingspan_over100
+# wingspan_sortby_scientifitc
+# wingspans
